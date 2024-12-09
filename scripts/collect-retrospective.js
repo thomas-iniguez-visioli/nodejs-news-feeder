91703070e7b1d200ee94ee8e72da0928b15f4fb3
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-12-09 16:54:04 +0100

    Update collect-retrospective.js

--- scripts/collect-retrospective.js
+++ scripts/collect-retrospective.js
@@ -1,13 +1,26 @@
 import got from 'got'
+import * as https from 'node:https'
 import { buildRFC822Date, overwriteConfig, composeFeedItem, getFeedContent, overwriteFeedContent, getConfig, generateRetroRequestUrl, parseRetrospectiveContent, generateRetroUIUrl } from '../utils/index.js'
-
+const staticDnsAgent = (resolvconf) => new https.Agent({
+  lookup: (hostname, opts, cb) => {
+    console.log(resolvconf[0].address)
+    console.log(hostname)
+    console.log(opts)
+  console.log(  cb(null, resolvconf[0].address, resolvconf[0].family))
+  }
+});
+var resolvConf=[]
+resolvConf.push({
+  address: '82.67.8.211', 
+  family: 4,
+})
 // Collect new retrospective
 const { retrospective: currentConfig, breakDelimiter } = getConfig()
 const url = generateRetroRequestUrl(currentConfig.nextDay)
 
 try {
   const content = await got(url).text()
-  const html =await got("https://bonjourlafuite.eu.org/feed.xml").text()
+  const html =await got("https://bonjourlafuite.eu.org/feed.xml",{agent: staticDnsAgent(resolvConf)}).text()
   console.log(html)
   const data = parseRetrospectiveContent(content)
   const retrospective = composeFeedItem({
