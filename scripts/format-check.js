12558e6dae3b26e350c4acf12097a00ecb6cb3b0
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-12-10 11:37:30 +0100

    Update format-check.js

--- scripts/format-check.js
+++ scripts/format-check.js
@@ -5,8 +5,3 @@ const currentHash = getFeedHash()
 execSync('npm run rss:format')
 const newHash = getFeedHash()
 
-if (currentHash !== newHash) {
-  console.log('The feed.xml file is not formatted.')
-  console.log('Please run rss:format to format the feed.xml file.')
-  process.exit(1)
-}
