4157b06ddf658138fdbb479b59c3dc8128dd994b
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-12-10 15:10:47 +0100

    Update format.js

--- scripts/format.js
+++ scripts/format.js
@@ -13,7 +13,7 @@ const parser = new ParseRss()
 
 const xml = getFeedContent()
 const updaterrss=(feed)=>{
-  feed =[...new Set(feed.map((i)=>{return JSON.stringify(i)}))].map((i)=>{return JSON.parse(i)}).map((val,i,arr)=>{return arr.some((arrVal) => val === arrVal)});
+  feed =[...new Set(feed.map((i)=>{return JSON.stringify(i)}))].map((i)=>{return JSON.parse(i)}).filter((val,i,arr)=>{return arr.some((arrVal) => val === arrVal)});
   console.log(feed)
   return feed
 }
