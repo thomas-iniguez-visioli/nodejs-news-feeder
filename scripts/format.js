4f8e24ade614e240f5e183cb72ada65f14925d61
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-12-12 13:36:58 +0100

    Update format.js

--- scripts/format.js
+++ scripts/format.js
@@ -26,7 +26,7 @@ const updaterrss=(feed)=>{
     return JSON.stringify(i)
   }))].map((i)=>{
     return JSON.parse(i)
-  }),"link"),"link")
+  }),"guid"),"link")
   //console.log(fed)
   return fed
 }
