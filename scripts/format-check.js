584d1553838beafd75e3cd1425950bda861a06f5
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-03-25 11:44:50 +0100

    Update format-check.js

--- scripts/format-check.js
+++ scripts/format-check.js
@@ -2,6 +2,8 @@ import { execSync } from 'child_process'
 import { getFeedHash } from '../utils/index.js'
 
 const currentHash = getFeedHash()
+try{
 execSync('npm run rss:format>>log.txt')
+}catch(error){console.log(error)}
 const newHash = getFeedHash()
 
