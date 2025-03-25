22d17fd8868a4c4097df7480bb61cbd35bed24d5
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-03-25 15:24:43 +0100

    Update build-website.js

--- scripts/build-website.js
+++ scripts/build-website.js
@@ -4,7 +4,9 @@ import { rssParse } from '@ulisesgascon/rss-feed-parser'
 
 const template = getWebsiteTemplate()
 const feed = getFeedContent()
+try{
 const { metadata, items } = rssParse(feed)
 console .log(items)
 const html = ejs.render(template, { metadata, items })
 overwriteWebsiteContent(html)
+}catch(err){console.log(err)}
