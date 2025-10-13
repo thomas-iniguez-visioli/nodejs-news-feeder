23295c14cba025e38266eaa96a1e98ba61450da9
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-10-13 14:12:24 +0200

    Update collect-retrospective.js

--- scripts/collect-retrospective.js
+++ scripts/collect-retrospective.js
@@ -78,7 +78,7 @@ addfeed("https://thomas-iniguez-visioli.github.io/retro-weekly/feed.xml")*/
       const contentItems = Array.from(contentList.querySelectorAll('li')).map(item => item.textContent);
       content = contentItems.join(', ');
     }
-    console.log(Array.from(entry.querySelectorAll('a')).filter((item)=>{return !item.id}).map((i)=>{return i.href}))
+    console.log(Array.from(entry.querySelectorAll('a')).filter((item)=>{return !item.id}).map((i)=>{return parse(i.toString())}))
     const source = "https://bonjourlafuite.eu.org/"+ entry.querySelector('a').getAttribute('href');
     return {
       timestamp:timestamp,
